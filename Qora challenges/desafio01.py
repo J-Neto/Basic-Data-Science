96e00c083a9c7aba39bd569b24dce0dcf168385a
José Neto <ribeirojoseph44@gmail.com>
2020-04-25 18:47:26 -0300

    adding comments

--- Qora challenges/desafio01.py
+++ Qora challenges/desafio01.py
@@ -3,8 +3,16 @@
 # construa um algoritmo que calcule seu peso ideal, 
 # usando a seguinte fórmula: (72.7*altura) - 58
 
+#Funcionamento: Haverá um menu de controle para que o Usuário escolha o que deseja realizar
+#1 para calcular, 2 para sair. Caso escolha uma opção inexistente, o programa avisa.
+
+#José Ribeiro Neto
+
+#Variável de controle
 exit = False
 print('Cálculo do Peso Ideal')
+
+#Loop do menu
 while(exit == False):
     op = int(input(('1- Calcular peso\n2- Sair do programa\nOpção: ')))
     
